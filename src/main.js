84d68e58a01b3dd4c6852fdac1e9645bb2ddda91
mineitan <mineitan@gmail.com>
2022-06-08 09:19:19 +0300

    Cors options added

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ const cors = require('cors')
 const server = express()
 server.use(express.static('views'))
 server.use(express.json())
-server.use(cors())
+
+const corsOptions = {
+    exposedHeaders: 'Access-Control-Allow-Origin'
+};
+
+server.use(cors(corsOptions))
+
+server.use((req,res,next) => {
+    res.set('Access-Control-Allow-Origin',"*")
+    next()
+})
+
 
 server.get("/", (req,res) => {
     res.sendFile("index.html")
